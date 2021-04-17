8218cfe033c8675608f147c317757c0d4d29feb2
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-17 22:44:30 +0530

    Add files via upload

---
+++ MyEcom/src/com/streamliners/Main.java
@@ -0,0 +1,40 @@
+package com.streamliners;
+
+import com.streamliners.models.Product;
+import com.streamliners.models.Variant;
+import com.streamliners.models.VariantBasedProduct;
+import com.streamliners.models.WeightBasedProduct;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+  //Create products
+  Product apple= new WeightBasedProduct("Apple","",10F,50F);
+  Product banana= new VariantBasedProduct("Banana","",new ArrayList(
+          Arrays.asList(
+                  new Variant("500gF",20f)
+        ,new Variant("1kgF",40f)
+          )
+  ));
+   //Add them in a list
+        List<Product> products= new ArrayList<>(
+                Arrays.asList(apple,banana)
+        );
+
+
+
+
+        System.out.println(products);
+
+
+
+    }
+
+
+
+
+}
